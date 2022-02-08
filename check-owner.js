871f544da196d505c0a1c4bb666d8c5f1ea2e6d2
Cameron Hejazi <chejazi@umich.edu>
2022-02-07 19:52:56 -0600

    add zorb contracts

--- check-owner.js
+++ check-owner.js
@@ -5,7 +5,8 @@ const networkID = 1;
 const infuraAccessToken = 'e8e7334c5daa415489ab1df636995565';
 const TOKENS = {
     CryptoPunks: '0xb47e3cd837ddf8e4c57f05d70ab865de6e193bbb',
-    BoredApes: '0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d'
+    BoredApes: '0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d',
+    Zorb: '0xca21d4228cdcc68d4e23807e5e370c07577dd152'
 };
 
 function getProvider() {
@@ -19,13 +20,18 @@ function getContract(address, provider) {
 
 (async () => {
     const provider = getProvider();
-    const latestBlock = await provider.getBlockNumber();
-    const baContract = getContract(TOKENS.BoredApes, provider);
-    const BAOwner = await baContract.ownerOf(1);
-    console.log(BAOwner);
+    // const baContract = getContract(TOKENS.BoredApes, provider);
+    // const BAOwner = await baContract.ownerOf(1);
+    // console.log(BAOwner);
 
-    const cpContract = getContract(TOKENS.CryptoPunks, provider);
-    const CPOwner = await cpContract.punkIndexToAddress(1);
-    console.log(CPOwner);
+    // const cpContract = getContract(TOKENS.CryptoPunks, provider);
+    // const CPOwner = await cpContract.punkIndexToAddress(1);
+    // console.log(CPOwner);
+
+
+    const zContract = getContract(TOKENS.Zorb, provider);
+    const zOwnerA = await zContract.ownerOf('25371');
+    const zOwnerB = await zContract.ownerOf('3466');
+    console.log(zOwnerA, zOwnerB);
 })();
 
