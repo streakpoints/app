9952c80435f60496061bc453cd806c62f287b0dc
Cameron Hejazi <chejazi@umich.edu>
2023-12-28 14:53:39 -0500

    Migrate to new SP contract.

--- server/app.js
+++ server/app.js
@@ -519,8 +519,11 @@ app.get('/-/api/checkin/verify', async (req, res) => {
   } else if (!req.session.account.verified) {
     jsonResponse(res, new Error('Not verified'));
   } else {
-    const signature = await blockchain.verifyCheckin(req.session.account.address);
-    jsonResponse(res, null, signature);
+    const { signature, currentEpoch } = await blockchain.verifyCheckin(req.session.account.address);
+    jsonResponse(res, null, {
+      verification: signature,
+      currentEpoch
+    });
   }
 });
 
